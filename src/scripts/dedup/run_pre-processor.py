94b2f36bcd8cb9a71b9dbfdc4c31d5010ca1df10
Sohail el Sabrouty <sohail.elsabrouty202@gmail.com>
2024-01-05 16:41:24 +0100

    Python script to execute complete pipeline
    Runs the whole pre-processing script

---
+++ src/scripts/dedup/run_pre-processor.py
@@ -0,0 +1,18 @@
+from tiltPreProcessor import tiltPreProcessor
+from tkinter import filedialog as fd
+import argparse
+import os
+
+# run the tiltPreProcessor.py script
+if __name__ == "__main__":
+    print('Select files to process (multiple files can be selected). Loading file dialog...')
+    filenames = fd.askopenfilenames()
+    print("{} file(s) selected".format(len(filenames)))
+    for filename in filenames:
+        print("Processing {}".format(os.path.basename(filename)))
+        src = filename
+        dest = str(os.path.join(os.path.dirname(os.path.dirname(filename)), "output/preprocessed_" + 
+                                os.path.basename(filename)).replace("_unprocessed", "")).replace("\\", "/")      
+        print(dest)
+        tiltPreProcessor(src, dest).pre_process()
+        print("Done\n")
